23e790e53f82270759018b82df07559c5b314387
Udit <23015406+udit-001@users.noreply.github.com>
2024-05-20 02:51:36 +0530

    feat: separate out MapCustomizerControl component

--- src/components/CountryMap.jsx
+++ src/components/CountryMap.jsx
@@ -11,7 +11,7 @@ import Loader from './Loader'
 import FixedBound from './FixedBound'
 import ImageExport from './ImageExport'
 import Description from './Description'
-import { POSITION_CLASSES } from '../utils'
+import MapCustomizerControl from './MapCustomizerControl'
 import MapCustomizer from './MapCustomizer'
 
 function MapEventController({ customizerControl }) {
@@ -20,20 +20,6 @@ function MapEventController({ customizerControl }) {
             customizerControl(false)
         }
     })
-}
-
-function CustomizerButton({ className, position, onPress }) {
-    const positionClass =
-        (position && POSITION_CLASSES[position]) || POSITION_CLASSES.topright
-    return (
-        <div className={`${className} ${positionClass}`}>
-            <div className="leaflet-control legend cursor-pointer bg-white rounded-sm outline outline-2 hover:bg-gray-100 outline-gray-400" onClick={(event) => { onPress(event) }} title="Edit Map">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 p-1">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                </svg>
-            </div>
-        </div>
-    )
 }
 
 
@@ -128,7 +114,7 @@ function CountryMap() {
                     <ZoomControl position='bottomright' />
                     <Description position='bottomleft' title="Click/Hover States" />
                     <ImageExport className="mt-9" position='topleft' fileName="India" />
-                    <CustomizerButton position='topleft' onPress={(event) => {
+                    <MapCustomizerControl position='topleft' onPress={(event) => {
                         setEditActive(true)
                     }} />
                 </MapContainer>
