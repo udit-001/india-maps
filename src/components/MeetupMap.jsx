f2e86ad9397b56ee3c616beaf5278cb4389a7049
Udit <23015406+udit-001@users.noreply.github.com>
2024-05-22 01:37:05 +0530

    fix: resolve bug preventing markers from loading

--- src/components/MeetupMap.jsx
+++ src/components/MeetupMap.jsx
@@ -3,77 +3,63 @@ import CountryMap from './CountryMap'
 import * as turf from "@turf/turf"
 
 function MeetupMap() {
-    const [data, setData] = useState({})
-    const [meetupCoords, setMeetupCoords] = useState([])
-    const [meetupData, setMeetupData] = useState({})
     const [markerData, setMarkerData] = useState([])
 
     useEffect(() => {
         const fetchData = async () => {
-            var response = await fetch("https://cdn.jsdelivr.net/gh/udit-001/india-maps-data@latest/geojson/india.geojson")
-            var result = await response.json()
-            setData(result)
-        }
-        fetchData()
-        const fetchMeetups = async () => {
-            var response = await fetch("/data/meetups.json")
-            var result = await response.json()
-            setMeetupData(result)
-        }
-        fetchMeetups()
-    }, [])
+            var indiaMapResponse = await fetch("https://cdn.jsdelivr.net/gh/udit-001/india-maps-data@latest/geojson/india.geojson")
+            var indiaMapData = await indiaMapResponse.json()
 
-    useEffect(() => {
-        var markers = []
-        var found = new Set();
-        var item, key, obj;
-        if (Object.keys(data).length !== 0) {
-            for (let i = 0; i < meetupData.length; i++) {
-                for (let j = 0; j < data.features.length; j++) {
-                    item = data.features[j]
-                    if ('district' in item['properties']) {
-                        key = item['properties']['district'].toLowerCase()
-                    }
-                    else {
-                        key = item['properties']['st_nm'].toLowerCase()
-                    }
-                    if (key.includes(meetupData[i]['region'].toLowerCase())) {
-                        if (!found.has(meetupData[i]['name'])) {
-                            obj = {
-                                ...meetupData[i],
-                                'coordinates': turf.center(item).geometry.coordinates.reverse()
+            var meetupResponse = await fetch("/data/meetups.json")
+            var meetupData = await meetupResponse.json()
+
+            var markers = []
+            var found = new Set();
+            var item, key, obj;
+
+            if (Object.keys(indiaMapData).length !== 0) {
+                for (let i = 0; i < meetupData.length; i++) {
+                    for (let j = 0; j < indiaMapData.features.length; j++) {
+                        item = indiaMapData.features[j]
+                        if ('district' in item['properties']) {
+                            key = item['properties']['district'].toLowerCase()
+                        }
+                        else {
+                            key = item['properties']['st_nm'].toLowerCase()
+                        }
+                        if (key.includes(meetupData[i]['region'].toLowerCase())) {
+                            if (!found.has(meetupData[i]['name'])) {
+                                obj = {
+                                    ...meetupData[i],
+                                    'coordinates': turf.center(item).geometry.coordinates.reverse()
+                                }
+                                markers.push(obj)
+                                found.add(meetupData[i]['name'])
                             }
-                            markers.push(obj)
-                            found.add(meetupData[i]['name'])
                         }
                     }
                 }
             }
+
+            var groups = new Set()
+            for (let i = 0; i < markers.length; i++) {
+                groups.add(markers[i]['tag'])
+            }
+
+            let output = {}
+            groups.forEach((group) => {
+                output[group] = []
+            })
+
+            markers.forEach((meetup) => {
+                output[meetup['tag']].push(meetup)
+            })
+
+            setMarkerData(Object.values(output))
+
         }
-        setMeetupCoords(markers)
-    }, [meetupData, setMeetupData])
-
-    useEffect(() => {
-        var groups = new Set()
-        for (let i = 0; i < meetupCoords.length; i++) {
-            groups.add(meetupCoords[i]['tag'])
-        }
-
-        let output = {}
-        groups.forEach((group) => {
-            output[group] = []
-        })
-
-        meetupCoords.forEach((meetup) => {
-            output[meetup['tag']].push(meetup)
-        })
-        var markerValues = Object.values(output)
-        setMarkerData(markerValues)
-    }, [meetupCoords, setMeetupCoords])
-
-    useEffect(() => {
-        console.log(markerData)
-    }, [markerData])
+        fetchData()
+    }, [])
 
     return (
         <CountryMap title="India Meetups" markers={markerData} featureEffects={false} />
